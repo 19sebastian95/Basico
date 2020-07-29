bb9e70bebed8f1bf78da231e31763781323da3aa
Sebastian Ramirez <desarrollador8@azsmart.net>
2020-07-29 18:49:16 -0500

    Updates

---
+++ precache-manifest.684293b0ab093def36fdf395438e6608.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf7586c88a09995684474a55369af5ff",
+    "url": "/Basico/index.html"
+  },
+  {
+    "revision": "b97eb3bbe5b3d2adb543",
+    "url": "/Basico/static/css/main.339083dc.chunk.css"
+  },
+  {
+    "revision": "0991bd5b3f9d9ca44a41",
+    "url": "/Basico/static/js/2.8f612d0d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Basico/static/js/2.8f612d0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b97eb3bbe5b3d2adb543",
+    "url": "/Basico/static/js/main.5c8a2606.chunk.js"
+  },
+  {
+    "revision": "a7feff9d0c1e720ca4c7",
+    "url": "/Basico/static/js/runtime-main.59e93f83.js"
+  }
+]);
